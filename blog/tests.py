b68d4b3c400e4a72ba5a7a0ef681d4867f96985c
Matt Walter <mwalter157@gmail.com>
2021-05-10 17:13:05 -0700

    Added blog, accounts and templates

--- blog/tests.py
+++ blog/tests.py
@@ -40,3 +40,28 @@ class BlogTests(TestCase):
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
 
+    def test_post_create_view(self):
+        response = self.client.post(reverse("post_new"), {
+            "title":  "New title",
+            "body": "New test",
+            "author": self.user.id
+        }) 
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Posts.objects.last().title, "New title")
+        self.assertEqual(Posts.objects.last().body, "New test")
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse("post_edit", args="1"), {
+            "title": "Update title",
+            "body": "Update test"
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
+
+
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, 
+    UpdateView,
+    DeleteView
+)
 from .models import Posts
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Posts
@@ -9,5 +15,20 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Posts
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Posts
+    template_name ="post_new.html"
+    fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Posts
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Posts
+    template_name="post_delete.html"
+    success_url = reverse_lazy("home")
 
 # Create your views here.
